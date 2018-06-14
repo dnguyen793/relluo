7033e1a546c9d289d833f449e0f3b227771f9e0d
Duy Nguyen <duyng93@gmail.com>
2018-06-13 18:28:54 -0700

    Add error messages to sign up page to match with log in page

--- server/public/js/sign_up.js
+++ server/public/js/sign_up.js
@@ -18,35 +18,45 @@ function initializeApp() {
 
 function getEmailInput() {
     var email = '';
+    var emailInput = $("#email");
+    var parent = $('.emailMsg');
+    var child = $('.emailMsg span');
     const emailValidification = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     $('.emailInput').on('keyup', function (event) {
         email = event.target.value;
+
         // console.log('email1:', email);
 
         if( !emailValidification.test(email) ){
 
-            var parent = $('.emailMsg');
-            var child = $('.emailMsg span');
             var text = 'Please enter a valid email address';
 
-            messageForInvalidInput( parent, child, text );
+            messageForInvalidInput( emailInput, parent, child, text );
 
             invalidEmail = true;
         }
         else{
             invalidEmail = false;
 
-            var parent = $('.emailMsg');
-            var child = $('.emailMsg span');
+            // var parent = $('.emailMsg');
+            // var child = $('.emailMsg span');
             var text = 'Valid email address';
 
-            messageForValidInput( parent, child, text );
+            messageForValidInput( emailInput, parent, child, text );
         }
     });
 
     $('.emailInput').on('focusout', function () {
         console.log('outfocus - email:', email);
+        if( !emailValidification.test(email) ){
+
+            var text = 'Please enter a valid email address';
+
+            messageForInvalidInput( emailInput, parent, child, text );
+
+            invalidEmail = true;
+        }
         return userEmail = email;
     });
 
@@ -54,30 +64,38 @@ function getEmailInput() {
 
 function getUsernameInput() {
     var username = '';
+    var usrnameInput = $("#username");
+    var parent = $('.usernameMsg');
+    var child = $('.usernameMsg span');
 
     $('.usernameInput').on('keyup', function (event) {
         username = event.target.value;
 
         if( username.length < 8 ){
 
-            var parent = $('.usernameMsg');
-            var child = $('.usernameMsg span');
+
             var text = 'Username must contain at least 8 characters';
 
-            messageForInvalidInput( parent, child , text);
+            messageForInvalidInput( usrnameInput, parent, child , text);
         }
         else{
-            var parent = $('.usernameMsg');
-            var child = $('.usernameMsg span');
+            // var parent = $('.usernameMsg');
+            // var child = $('.usernameMsg span');
             var text = 'Valid username';
 
-            messageForValidInput( parent, child , text);
+            messageForValidInput( usrnameInput, parent, child , text);
         }
     });
 
     $('.usernameInput').on('focusout', function () {
         console.log('outfocus - username:', username);
+        if( username.length < 8 ){
 
+
+            var text = 'Username must contain at least 8 characters';
+
+            messageForInvalidInput( usrnameInput, parent, child , text);
+        }
         return userName = username;
 
     });
@@ -85,40 +103,41 @@ function getUsernameInput() {
 
 function getPasswordInput() {
     var pwd = '';
+    var pwdInput = $("#password");
+    var parent = $('.pwdMsg');
+    var child = $('.pwdMsg span');
 
     $('.pwdInput').on('keyup', function (event) {
         pwd = event.target.value;
-        console.log(pwd);
-        // var sha  = sha1(pwd);
-        // console.log(sha);
-        console.log('pwd.length:', pwd.length);
 
         if( pwd.length < 8 ){
 
-            var parent = $('.pwdMsg');
-            var child = $('.pwdMsg span');
             var text = 'Password must contains at least 8 characters';
 
-            messageForInvalidInput( parent, child, text );
+            messageForInvalidInput( pwdInput, parent, child, text );
         }
         else{
-
-            var parent = $('.pwdMsg');
-            var child = $('.pwdMsg span');
             var text = 'Valid password';
 
-            messageForValidInput( parent, child, text );
+            messageForValidInput( pwdInput, parent, child, text );
         }
     });
 
     $('.pwdInput').on('focusout', function () {
         console.log('outfocus - pwd:', pwd);
+        if( pwd.length < 8 ){
+
+            var text = 'Password must contains at least 8 characters';
+
+            messageForInvalidInput( pwdInput, parent, child, text );
+        }
         return userPwd = pwd;
     });
 }
 
-function messageForInvalidInput( parentElement, childElement, text ) {
-    parentElement.addClass('invalidInput').removeClass('validInput');
+function messageForInvalidInput( inputfield, parentElement, childElement, text ) {
+    parentElement.removeClass('validInput').addClass('invalidInput');
+    inputfield.removeClass('validInput').addClass('invalidInput');
     var icon = $("<i>", {
         "class": "material-icons tiny valign-wrapper",
         text: "error_outline"
@@ -127,8 +146,10 @@ function messageForInvalidInput( parentElement, childElement, text ) {
     childElement.append(icon);
 }
 
-function messageForValidInput( parentElement, childElement, text) {
+function messageForValidInput( inputfield, parentElement, childElement, text) {
     parentElement.removeClass('invalidInput').addClass('validInput');
+    inputfield.removeClass('invalidInput').addClass('validInput');
+
     var icon = $("<i>", {
         "class": "material-icons tiny valign-wrapper",
         text: "check"
@@ -173,6 +194,9 @@ function handleSignUpBtnClick() {
 
     if( userEmail === undefined || userName === undefined || userPwd === undefined || userName.length < 8 || userPwd.length < 8 || invalidEmail ){
         $('.message span').text("Please correct the error/errors above and try again").addClass('invalidInput');
+        setInterval( () => {
+            $('.message span').text('');
+        }, 3500);
         return;
     }
     else {
